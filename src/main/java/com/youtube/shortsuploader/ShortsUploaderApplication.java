4a981a63a0851ed153a3f728a41f8471a3bcb1d1
nick <andantej281@gmail.com>
2025-07-08 19:10:19 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/youtube/shortsuploader/ShortsUploaderApplication.java
@@ -0,0 +1,12 @@
+package com.youtube.shortsuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortsUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortsUploaderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/youtube/shortsuploader/ShortsUploaderApplicationTests.java
@@ -0,0 +1,11 @@
+package com.youtube.shortsuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortsUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
